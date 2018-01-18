f587ab68fa5a6b05ac8f0b9eb289cbdb70717118
potato_farmer <wildtangles@gmail.com>
2018-01-18 13:24:51 +0000

    added quick web demo

--- Analytics.py
+++ Analytics.py
@@ -11,40 +11,43 @@ import os
 import CustomConfiguration
 import NewJob
 import multiprocessing
+import shutil
 
-nlep_val = 0            #initialize a string for number of leptons
-LepTmass_val = 0        #lepton min transverse mass
-LepTmassMax_val = 200   #lepton max transverse mass
-TwoLepcharge_val = 1    #2 Leptons: same/diferent charge
-TwoLepflavour_val = 1   #2 Leptons: same/diferent flavour
-InvariantM_val = 0      #invariant mass
-InvariantM2_val = 0     #invariant mass
-Range_val = 0           #range of invariant mass
-st_lepchargecb = 0      #lepton charge
-st_lepflavourcb = 0     #lepton flavour
-st_InvMasscb = 0        #invariant mass of pair
-leppt_val = 25          #lepton momentum
-st_lepptcb = 0          #state of checkbox
-minnjet_val = 0         #initialize integer for min number of jets
-maxnjet_val = 9         #initialize integer for max number of jets
-btagmin_val = 0         #initialise minimum b-jets
-btagmax_val = 9         #initialise maximum b-jets
-st_btagjetcb = 0        #state of checkbox
-minmissE_val = 0        #concerning missing min momentum
-maxmissE_val = 200      #concerning missing max momentum
-percentg_val = 0.5      #percentage of data to analyze, from 0 to 100
-st_lepcb = 0            #state of checkbox: choose number of charged leptons, 1 = checked 0 = unchecked
-st_jetcb = 0            #state of checkbox: choose number of jets
-st_missPcb = 0          #state of checkbox: missing transverse momentum (GeV)
+# nlep_val = 0            #initialize a string for number of leptons
+# LepTmass_val = 0        #lepton min transverse mass
+# LepTmassMax_val = 200   #lepton max transverse mass
+# TwoLepcharge_val = 1    #2 Leptons: same/diferent charge
+# TwoLepflavour_val = 1   #2 Leptons: same/diferent flavour
+# InvariantM_val = 0      #invariant mass
+# InvariantM2_val = 0     #invariant mass
+# Range_val = 0           #range of invariant mass
+# st_lepchargecb = 0      #lepton charge
+# st_lepflavourcb = 0     #lepton flavour
+# st_InvMasscb = 0        #invariant mass of pair
+# leppt_val = 25          #lepton momentum
+# st_lepptcb = 0          #state of checkbox
+# minnjet_val = 0         #initialize integer for min number of jets
+# maxnjet_val = 9         #initialize integer for max number of jets
+# btagmin_val = 0         #initialise minimum b-jets
+# btagmax_val = 9         #initialise maximum b-jets
+# st_btagjetcb = 0        #state of checkbox
+# minmissE_val = 0        #concerning missing min momentum
+# maxmissE_val = 200      #concerning missing max momentum
+# percentg_val = 0.5      #percentage of data to analyze, from 0 to 100
+# st_lepcb = 0            #state of checkbox: choose number of charged leptons, 1 = checked 0 = unchecked
+# st_jetcb = 0            #state of checkbox: choose number of jets
+# st_missPcb = 0          #state of checkbox: missing transverse momentum (GeV)
 
 latestThread = None
 histograms =[]
 pool= None
+analysisComplete = False
 
-def run_analysis(_st_jetcb,_minnjet_val,_maxnjet_val,_st_btagjetcb,_btagmin_val, _btagmax_val, _st_lepcb, _nlep_val,_st_lepptcb, _leppt_val, _LepTmass_val, _LepTmassMax_val, _st_lepchargecb, _TwoLepcharge_val, _st_lepflavourcb, _TwoLepflavour_val, _st_InvMasscb, _InvariantM_val, _Range_val, _InvariantM2_val, _minmissE_val, _maxmissE_val, _st_missPcb, _percentg_val):
+def run_analysis(_nlep_val, _LepTmass_val, _LepTmassMax_val, _TwoLepcharge_val, _TwoLepflavour_val, _InvariantM_val, _InvariantM2_val, _Range_val, _st_lepchargecb, _st_lepflavourcb, _st_InvMasscb, _leppt_val, _st_lepptcb, _minnjet_val, _maxnjet_val, _btagmin_val, _btagmax_val, _st_btagjetcb, _minmissE_val, _maxmissE_val, _percentg_val, _st_lepcb, _st_jetcb, _st_missPcb):
     """runs the analysis"""
-    global makeplots #draw the plots
-    makeplots = True
+
+    global analysisComplete
+    analysisComplete = False
 
     selection = []
     global histograms
@@ -207,22 +210,18 @@ def run_analysis(_st_jetcb,_minnjet_val,_maxnjet_val,_st_btagjetcb,_btagmin_val,
     for process in pool:
         process.join()
 
-
-    global k
-    k = 0
-    global runpressed
-    if not makeplots:
-        runpressed = False
-        return
-
-    previousplots=glob.glob('Output/*.gif')
-    for plot in previousplots: 
-        os.remove(plot)
+    # previousplots=glob.glob('Output/*.gif')
+    # for plot in previousplots:
+    #     os.remove(plot)
 
     if not histograms == []:
         NewPlotResults.plot_results(histograms)
 
-    runpressed = False
+    for histogram in glob.glob("Output/*.gif"):
+        shutil.copy(histogram, 'static/histograms')
+
+
+    analysisComplete = True
     
 class JobPool(multiprocessing.Process):
     """Process object for running a job"""
@@ -237,26 +236,33 @@ class JobPool(multiprocessing.Process):
 class run_thread(threading.Thread):#MARK
     """thread object for running the analysis"""
     
-    def __init__(self):
+    def __init__(self, params):
         super(run_thread,self).__init__()
+        self.params = params
 
     def run(self):
-        run_analysis(st_jetcb, minnjet_val, maxnjet_val, st_btagjetcb, btagmin_val, btagmax_val, st_lepcb,
-                     nlep_val, st_lepptcb, leppt_val, LepTmass_val, LepTmassMax_val, st_lepchargecb, TwoLepcharge_val,
-                     st_lepflavourcb, TwoLepflavour_val, st_InvMasscb, InvariantM_val, Range_val, InvariantM2_val,
-                     minmissE_val, maxmissE_val, st_missPcb, percentg_val)
+        # run_analysis(st_jetcb, minnjet_val, maxnjet_val, st_btagjetcb, btagmin_val, btagmax_val, st_lepcb,
+        #              nlep_val, st_lepptcb, leppt_val, LepTmass_val, LepTmassMax_val, st_lepchargecb, TwoLepcharge_val,
+        #              st_lepflavourcb, TwoLepflavour_val, st_InvMasscb, InvariantM_val, Range_val, InvariantM2_val,
+        #              minmissE_val, maxmissE_val, st_missPcb, percentg_val)
+        run_analysis(*self.params)
               
 runpressed = False
     
-def run_a():
+def run_a(params):
     """creates an analysis therad"""
-    global runpressed
-    runpressed = True
 
+    print(params)
+
+    global analysisComplete
+    analysisComplete = False
     global latestThread
-    latestThread =run_thread()
-    #latestThread.setDaemon(True)
+    latestThread =run_thread(params)
+    latestThread.setDaemon(True)
     latestThread.start()
+
+def get_status():
+    return analysisComplete
 
 if __name__ == '__main__':
     run_a()
---
+++ Web.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect, flash
+from Analytics import run_a, get_status
+import glob
+import os
+import shutil
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def whatisthis():
+    return render_template('whatisthis.html')
+
+@app.route('/analytics', methods = ['GET', 'POST'])
+def analytics():
+    if request.method == 'POST':
+        params = [ float(param_string) for param_string in request.form.getlist('cut[]') ]
+        print(params[-4])
+        #params = [0, 0, 200, 1, 1, 0, 0, 0, 0, 0, 0, 25, 0, 0, 9, 0, 9, 0, 0, 200, float(request.form.getlist('cut[]')[0]), 0, 0, 0]
+        for oldHistogram in glob.glob("static/histograms/*.gif"):
+            os.remove(oldHistogram)
+        for oldHistogram in glob.glob("Output/*.gif"):
+            os.remove(oldHistogram)
+        run_a(params)
+        while not get_status():
+            pass
+        return render_template('analytics.html', histograms=[histogram+'?no-cache-token={}'.format(time.time()) for histogram in glob.glob("static/histograms/*.gif")])
+    else:
+        return render_template('analytics.html', histograms=[])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
